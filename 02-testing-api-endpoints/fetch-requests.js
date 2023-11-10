cec99df2b284fe4c216dc98f84543efd6a0394bd
tristan <thuckabee.work@gmail.com>
2023-11-09 18:47:16 -0600

    east 01-02

--- 02-testing-api-endpoints/fetch-requests.js
+++ 02-testing-api-endpoints/fetch-requests.js
@@ -8,15 +8,21 @@ Paste your code for fetch requests here once you finish each task.
   Make a request with fetch request to GET /posts and print the response
   components to the console.
 */
-
 // Your code here
-
-
+fetch('/posts')
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
 
 /* =============================== Phase 2 ================================ */
 /*
   Make a request with fetch request to POST /posts and print the response
   components to the console.
 */
-
 // Your code here
+fetch('/posts', {
+  method: 'POST',
+  headers: {'Content-Type':'application/json'},
+  body: JSON.stringify({message: 'new post'})
+})
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
